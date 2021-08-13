dad81d15cba223adabbc86e23d33b8b2699ca383
Ishani Kohli <ishanikohli2001@gmail.com>
2021-08-13 17:10:04 +0530

    help command done

---
+++ commands/help.js
@@ -0,0 +1,11 @@
+function help(){
+    console.log(`\nList of all the commands 
+    👉 node main.js view "path" flat
+    👉 node main.js view "path" tree
+    👉 node main.js organize "path"
+    👉 node main.js help`)
+}
+
+module.exports={
+    helpFn:help
+}
